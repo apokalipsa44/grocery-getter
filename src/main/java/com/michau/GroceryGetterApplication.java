b6fd27ece9a2d2641350fe9cd6a85957112ce933
apokalipsa44 <m_lachowicz@wp.pl>
2019-08-15 10:46:39 +0200

    initial commit

---
+++ src/main/java/com/michau/GroceryGetterApplication.java
@@ -0,0 +1,13 @@
+package com.michau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryGetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryGetterApplication.class, args);
+	}
+
+}
