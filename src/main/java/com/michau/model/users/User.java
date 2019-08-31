0180681d9a4025df3c5c18abf1d1777158161ccb
apokalipsa44 <m_lachowicz@wp.pl>
2019-08-31 13:35:50 +0200

    fixed Designer

--- src/main/java/com/michau/model/users/User.java
+++ src/main/java/com/michau/model/users/User.java
@@ -20,5 +20,53 @@ public class User {
 
     private String role;
 
+    public User() {
+    }
 
+    public User(String name, String password, String email, String role) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
