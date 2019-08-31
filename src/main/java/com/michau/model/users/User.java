a46d4bba453479af5ff3f71c9fd7e386daa19b93
apokalipsa44 <m_lachowicz@wp.pl>
2019-08-31 12:23:19 +0200

    kk

--- src/main/java/com/michau/model/users/User.java
+++ src/main/java/com/michau/model/users/User.java
@@ -20,13 +20,5 @@ public class User {
 
     private String role;
 
-    public User() {
-    }
 
-    public User(String name, String password, String email, String role) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-    }
 }
