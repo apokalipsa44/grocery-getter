4e665890d53072cc6392e2dec100dbef563d1c75
apokalipsa44 <m_lachowicz@wp.pl>
2019-08-31 12:00:18 +0200

    user class

---
+++ src/main/java/com/michau/model/users/User.java
@@ -0,0 +1,22 @@
+package com.michau.model.users;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private String role;
+}
